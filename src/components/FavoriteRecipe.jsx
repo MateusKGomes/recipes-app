7510be73665f04d3c9e3ff07104bd579e79e8a25
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-03-09 17:43:05 -0300

    Requisitos 34 e 35

--- src/components/FavoriteRecipe.jsx
+++ src/components/FavoriteRecipe.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import context from '../context/RecipesContext';
 
 function FavoriteRecipe() {
   const style = {
     marginBottom: '30px',
     bottom: 0,
+  };
+
+  const { recipesDetails } = useContext(context);
+  const history = useHistory();
+  const pathName = history.location.pathname.includes('meals') ? 'meals' : 'drinks';
+  const id = history.location.pathname.includes('meals')
+    ? recipesDetails[pathName][0]?.idMeal
+    : recipesDetails[pathName][0]?.idDrink;
+  const image = history.location.pathname.includes('meals')
+    ? recipesDetails[pathName][0]?.strMealThumb
+    : recipesDetails[pathName][0]?.strDrinkThumb;
+  const name = history.location.pathname.includes('meals')
+    ? recipesDetails[pathName][0]?.strMeal
+    : recipesDetails[pathName][0]?.strDrink;
+  const alcoholicOrNot = history.location.pathname.includes('meals')
+    ? ''
+    : recipesDetails[pathName][0]?.strAlcoholic;
+  const nationality = history.location.pathname.includes('meals')
+    ? recipesDetails[pathName][0]?.strArea
+    : '';
+  const type = history.location.pathname.includes('meals')
+    ? 'meal' : 'drink';
+  console.log(recipesDetails);
+  console.log(image);
+  const saveLocal = () => {
+    const favorite = localStorage
+      .getItem('favoriteRecipes')
+      ? JSON.parse(localStorage.getItem('favoriteRecipes')) : [];
+    localStorage.setItem('favoriteRecipes', JSON
+      .stringify(
+        [
+          ...favorite,
+          {
+            id,
+            type,
+            nationality,
+            category: recipesDetails[pathName][0].strCategory,
+            alcoholicOrNot,
+            name,
+            image,
+          }],
+      ));
   };
 
   return (
@@ -12,6 +56,7 @@ function FavoriteRecipe() {
         type="button"
         data-testid="favorite-btn"
         style={ style }
+        onClick={ saveLocal }
       >
         Favoritar
       </button>
--- src/components/ShareRecipe.jsx
+++ src/components/ShareRecipe.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import copy from 'clipboard-copy';
+import React, { useState } from 'react';
+import shareIcon from '../images/shareIcon.svg';
 
 function ShareRecipe() {
+  const [copyMessage, setCopyMessage] = useState(false);
+
+  const message = () => {
+    copy(window.location.href);
+    setCopyMessage(true);
+  };
   return (
     <div>
       <button
         type="button"
         data-testid="share-btn"
+        onClick={ message }
       >
-        Compartilhar
+        <img
+          src={ shareIcon }
+          alt="Compartilhar"
+        />
       </button>
+      {
+        copyMessage && <p>Link copied!</p>
+      }
     </div>
   );
 }
