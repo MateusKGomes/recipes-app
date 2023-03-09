99ca11f8256432e42d21d55ba9eda7a8e309cbdc
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-03-09 20:00:46 -0300

    Requisito 35 e 36

--- src/components/FavoriteRecipe.jsx
+++ src/components/FavoriteRecipe.jsx
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import context from '../context/RecipesContext';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipe() {
   const style = {
@@ -8,12 +10,19 @@ function FavoriteRecipe() {
     bottom: 0,
   };
 
+  const [isFavorite, setIsFavorite] = useState(false);
+
   const { recipesDetails } = useContext(context);
+
   const history = useHistory();
   const pathName = history.location.pathname.includes('meals') ? 'meals' : 'drinks';
-  const id = history.location.pathname.includes('meals')
-    ? recipesDetails[pathName][0]?.idMeal
-    : recipesDetails[pathName][0]?.idDrink;
+
+  const location = useLocation();
+  const params = location.pathname.split('/');
+  const id = (params[params.length - 1]);
+  //   const id = history.location.pathname.includes('meals')
+  //     ? recipesDetails[pathName][0]?.idMeal
+  //     : recipesDetails[pathName][0]?.idDrink;
   const image = history.location.pathname.includes('meals')
     ? recipesDetails[pathName][0]?.strMealThumb
     : recipesDetails[pathName][0]?.strDrinkThumb;
@@ -28,37 +37,57 @@ function FavoriteRecipe() {
     : '';
   const type = history.location.pathname.includes('meals')
     ? 'meal' : 'drink';
-  console.log(recipesDetails);
-  console.log(image);
+
   const saveLocal = () => {
     const favorite = localStorage
       .getItem('favoriteRecipes')
       ? JSON.parse(localStorage.getItem('favoriteRecipes')) : [];
-    localStorage.setItem('favoriteRecipes', JSON
-      .stringify(
-        [
-          ...favorite,
-          {
-            id,
-            type,
-            nationality,
-            category: recipesDetails[pathName][0].strCategory,
-            alcoholicOrNot,
-            name,
-            image,
-          }],
-      ));
+
+    if (favorite
+      .some((el) => +el.id === +id)) {
+      localStorage.setItem('favoriteRecipes', JSON
+        .stringify(favorite.filter((item) => +item.id !== +id)));
+      setIsFavorite(false);
+    } else {
+      localStorage.setItem('favoriteRecipes', JSON
+        .stringify(
+          [
+            ...favorite,
+            {
+              id,
+              type,
+              nationality,
+              category: recipesDetails[pathName][0].strCategory,
+              alcoholicOrNot,
+              name,
+              image,
+            }],
+        ));
+      setIsFavorite(true);
+    }
   };
 
+  useEffect(() => {
+    const favorite = localStorage
+      .getItem('favoriteRecipes')
+      ? JSON.parse(localStorage.getItem('favoriteRecipes')) : [];
+    if (favorite
+      .some((el) => +el.id === +id)) {
+      setIsFavorite(true);
+    } else {
+      setIsFavorite(false);
+    }
+  }, []);
+
+  const heartIcon = isFavorite ? blackHeartIcon : whiteHeartIcon;
   return (
     <div>
       <button
         type="button"
-        data-testid="favorite-btn"
         style={ style }
         onClick={ saveLocal }
       >
-        Favoritar
+        <img data-testid="favorite-btn" src={ heartIcon } alt="toggle-favorite" />
       </button>
     </div>
   );
